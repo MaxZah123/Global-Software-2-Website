491c924deee97a7f1de1404e2c44bdea628fe210
Meapy <dkrasovskijj@gmail.com>
2020-04-17 15:52:57 +0100

    update

--- websiteproj/settings.py
+++ websiteproj/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -60,6 +61,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                'django.template.context_processors.i18n',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -113,6 +115,17 @@ USE_L10N = True
 
 USE_TZ = True
 
+from django.utils.translation import ugettext_lazy as _
+
+LANGUAGES = (
+
+    ('en', _('English')),
+
+    ('de', _('German')),
+
+    ('ru', _('Russian')),
+)
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
@@ -120,7 +133,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-
-os.path.join(BASE_DIR, 'websiteproj/static'),
-
+    os.path.join(BASE_DIR, 'websiteproj/static'),
 ]
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'websiteproj/locale'),
+)
